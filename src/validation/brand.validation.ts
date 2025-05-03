60823ca39a49894bdb7748fe560e6ccc9ad6ec5c
Emper <minhtruongb16@gmail.com>
2025-05-04 00:34:00 +0700

    Update Product Api

--- src/validation/brand.validation.ts
+++ src/validation/brand.validation.ts
@@ -4,8 +4,7 @@ export class BrandValidation {
   static createBrand() {
     return {
       body: z.object({
-        name: z.string().nonempty('Tên thương hiệu không được để trống'),
-        description: z.string().optional(),
+        brand_name: z.string().nonempty('Tên thương hiệu không được để trống'),
         country: z.string().optional(),
         logo_url: z.string().optional()
       })
@@ -15,8 +14,8 @@ export class BrandValidation {
   static updateBrand() {
     return {
       body: z.object({
-        name: z.string().optional(),
-        description: z.string().optional(),
+        brand_name: z.string().optional(),
+        brand_image: z.string().optional(),
         country: z.string().optional(),
         logo_url: z.string().optional()
       })
