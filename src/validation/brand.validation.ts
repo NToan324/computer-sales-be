29ce6f357e93ef38c41221af13530d1d92866fff
StevePenguin <dogiahuy2252004@gmail.com>
2025-05-05 00:43:04 +0700

    fix some logic in product, create brand services, syncES func

--- src/validation/brand.validation.ts
+++ src/validation/brand.validation.ts
@@ -7,7 +7,7 @@ export class BrandValidation {
         brand_name: z.string().nonempty('Tên thương hiệu không được để trống'),
         brand_image: z.object({
           url: z.string().nonempty('URL không được để trống'),
-          public_id: z.string()
+          public_id: z.string().optional()
         }),
       })
     }
