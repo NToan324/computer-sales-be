2276ef2288e7f0b0c43d345dcbdb3034e2dd02dd
StevePenguin <dogiahuy2252004@gmail.com>
2025-05-16 15:16:54 +0700

    fix error

--- src/queue/mail.queue.ts
+++ src/queue/mail.queue.ts
@@ -1,19 +1,19 @@
-import { Queue } from 'bull';
-import { redisClient } from '@/config/redis'; // Adjust the import based on your Redis configuration
-import { sendEmail } from '@/utils/mailer';
+// import { Queue } from 'bull';
+// import { redisClient } from '@/config/redis'; // Adjust the import based on your Redis configuration
+// import { sendEmail } from '@/utils/mailer';
 
-const emailQueue = new Queue('email', {
-    redis: {
-        host: redisClient.host,
-        port: redisClient.port,
-    },
-});
+// const emailQueue = new Queue('email', {
+//     redis: {
+//         host: redisClient.host,
+//         port: redisClient.port,
+//     },
+// });
 
-emailQueue.process(async (job) => {
-    const { to, subject, text } = job.data;
-    await sendEmail(to, subject, text);
-});
+// emailQueue.process(async (job) => {
+//     const { to, subject, text } = job.data;
+//     await sendEmail(to, subject, text);
+// });
 
-export const addEmailToQueue = (to: string, subject: string, text: string) => {
-    emailQueue.add({ to, subject, text });
-};
+// export const addEmailToQueue = (to: string, subject: string, text: string) => {
+//     emailQueue.add({ to, subject, text });
+// };
