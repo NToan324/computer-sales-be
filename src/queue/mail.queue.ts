923c94852b40cbf71a56189d4f4e577db806138e
NToan324 <nhattoan664t@gmail.com>
2025-05-17 11:50:47 +0700

    pull

--- src/queue/mail.queue.ts
+++ src/queue/mail.queue.ts
@@ -1,7 +1,8 @@
-import Queue from 'bull';
-import { redisConfig } from '@/config/redis';
+import Queue from 'bull'
+import { redisConfig } from '@/config/redis'
 
 // Lấy URL từ client options
-const redisUrl = redisConfig.getClient().options?.url || 'redis://localhost:6379';
+const redisUrl =
+    redisConfig.getClient().options?.url || 'redis://localhost:6379'
 
-export const mailQueue = new Queue('mailQueue', redisUrl);
+export const mailQueue = new Queue('mailQueue', redisUrl)
